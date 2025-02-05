d78537d4c730a8c9398c1ec2fcbef6ba687ac4aa
Stephanie Frost <froststep69@gmail.com>
2025-02-05 14:11:51 +0100

    hero start

--- js/index.js
+++ js/index.js
@@ -2,3 +2,21 @@
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
+
+
+let heroImg = document.createElement("div")
+heroImg.classList.add("heroImg")
+heroImg.innerHTML = `
+<img src="${hero.image}" alt="lady in Steph rose (thats a colour i have proof)">
+`
+
+document.body.append(heroImg)
+
+let exploreBox = document.createElement("div")
+exploreBox.classList.add("exploreBox")
+exploreBox.innerHTML = `
+<h1>${hero.headline}</h1>
+<p>${hero.copy}</p>
+`
+
+document.body.append(exploreBox)
