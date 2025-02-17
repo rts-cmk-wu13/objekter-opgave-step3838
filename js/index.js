e1d266b86c104d04d522dcc3c3060b855b6c2b49
Stephanie Frost <froststep69@gmail.com>
2025-02-17 08:53:07 +0100

    notes to devide

--- js/index.js
+++ js/index.js
@@ -3,20 +3,23 @@
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
 
-
+/* Hero img */
 let heroImg = document.createElement("div")
 heroImg.classList.add("heroImg")
 heroImg.innerHTML = `
 <img src="${hero.image}" alt="lady in Steph rose (thats a colour i have proof)">
 `
-
 document.body.append(heroImg)
+/* --- */
 
+
+
+/* Explore box */
 let exploreBox = document.createElement("div")
 exploreBox.classList.add("exploreBox")
 exploreBox.innerHTML = `
 <h1>${hero.headline}</h1>
 <p>${hero.copy}</p>
 `
-
 document.body.append(exploreBox)
+/* --- */
